6dec664a3b43f0d55f0000d2ee18f6339ac98c07
JWYU0006 <jwyu0006@gmail.com>
2026-02-02 20:48:44 -0500

    Sleep test

--- Assets/Scripts/CatNeeds.cs
+++ Assets/Scripts/CatNeeds.cs
@@ -4,4 +4,14 @@ public class CatNeeds : MonoBehaviour
 {
     public float hungry;
     public float thirsty;
+    public float energy;
+    public Transform body;
+
+    private void Update()
+    {
+        if (energy <= 50)
+        {
+            body.rotation = Quaternion.Euler(90, 0, 0);
+        }
+    }
 }
