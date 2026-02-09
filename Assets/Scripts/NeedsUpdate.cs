c79a5ee54b74a114da83b8cd25408f6df04794cf
JWYU0006 <jwyu0006@gmail.com>
2026-02-08 20:51:16 -0500

    Needs update and constrain

--- Assets/Scripts/NeedsUpdate.cs
+++ Assets/Scripts/NeedsUpdate.cs
@@ -1,20 +1,23 @@
 using NodeCanvas.Framework;
 using UnityEngine;
 
-
 namespace NodeCanvas.Tasks.Actions
 {
-
     public class NeedsUpdate : ActionTask
     {
         public BBParameter<float> satietyBBP = 100f;
         public BBParameter<float> thirstyBBP = 100f;
         public BBParameter<float> energyBBP = 100f;
         public BBParameter<float> cleanlinessBBP = 100f;
-        public BBParameter<float> satietySpeedBBP = 0.1f;
-        public BBParameter<float> thirstySpeedBBP = 0.1f;
-        public BBParameter<float> energySpeedBBP = 0.1f;
-        public BBParameter<float> cleanlinessSpeedBBP = 0.1f;
+        public BBParameter<float> satietySpeedBBP = 4f;
+        public BBParameter<float> thirstySpeedBBP = 2f;
+        public BBParameter<float> energySpeedBBP = 1f;
+        public BBParameter<float> cleanlinessSpeedBBP = 4f;
+
+        protected override void OnUpdate()
+        {
+            NeedsUpdateFunction();
+        }
 
         public void NeedsUpdateFunction()
         {
@@ -22,26 +25,27 @@ namespace NodeCanvas.Tasks.Actions
             float thirsty = thirstyBBP.value;
             float energy = energyBBP.value;
             float cleanliness = cleanlinessBBP.value;
-            if (satiety < 20f)
-            {
-                // Handle low satiety
-            }
-            if (thirsty < 20f)
-            {
-                // Handle low thirst
-            }
-            if (energy < 20f)
-            {
-                // Handle low energy
-            }
-            if (cleanliness < 20f)
-            {
-                // Handle low cleanliness
-            }
+
+            satiety = UpdateAndConstrain(satiety, "satiety");
+            satietyBBP.value = satiety;
+            thirsty = UpdateAndConstrain(thirsty, "thirsty");
+            thirstyBBP.value = thirsty;
+            energy = UpdateAndConstrain(energy, "energy");
+            energyBBP.value = energy;
+            cleanliness = UpdateAndConstrain(cleanliness, "cleanliness");
+            cleanlinessBBP.value = cleanliness;
         }
 
-        private float UpdateAndConstrain(float need)
+        private float UpdateAndConstrain(float need, string needName)
         {
+            if (needName == "satiety")
+                need -= satietySpeedBBP.value * Time.deltaTime;
+            else if (needName == "thirsty")
+                need -= thirstySpeedBBP.value * Time.deltaTime;
+            else if (needName == "energy")
+                need -= energySpeedBBP.value * Time.deltaTime;
+            else if (needName == "cleanliness")
+                need -= cleanlinessSpeedBBP.value * Time.deltaTime;
 
             return Mathf.Clamp(need, 0f, 100f);
         }
